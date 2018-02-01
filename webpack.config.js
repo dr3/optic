ad3ca439f5424532c4809c9163fda185a5a165c1
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-02-01 16:12:48 +0000

    Setup code and dev enviroment with webpack

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const production = process.env.NODE_ENV == 'production';
+
+const pkg = require('./package.json');
+
+var plugins = [];
+
+var loaders = [
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader'
+  }
+];
+
+if (production) {
+  loaders = [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }
+  ];
+
+  plugins = [
+    new UglifyJSPlugin({
+      sourceMap: true,
+      mangle: true,
+      compress: {
+        screw_ie8: true
+      }
+    }),
+    
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+
+    new webpack.BannerPlugin({
+      banner: `
+        ${pkg.title || pkg.name} - v${pkg.version}
+        ${pkg.description}
+        ${pkg.homepage}
+        
+        Made by ${pkg.author.name} - ${pkg.author.url}
+        Under ${pkg.license} License
+      `
+    })
+  ];
+}
+
+module.exports = {
+  devtool: production ? false : 'source-map',
+
+  entry: './src/optic',
+
+  devServer: {
+    contentBase: './demo'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'optic.js',
+    libraryTarget: 'umd',
+    library: 'optic',
+    libraryExport: 'default'
+  },
+
+  module: {
+    rules: loaders
+  },
+
+  plugins
+
+};
